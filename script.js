b24f84df37c54a43b47ba8b0237465979e2c0926
Vivance <114446365+vivanceA@users.noreply.github.com>
2023-10-12 21:02:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add this to your script.js file
+
+// Smooth scroll navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
